f61aed14a41603a520e63902c2f9b7c2eb061085
erik aronesty <erik@q32.com>
2018-11-28 10:47:11 -0500

    .

---
+++ upload.sh
@@ -0,0 +1,8 @@
+# example build 
+
+export CONAN_UPLOAD="https://api.bintray.com/conan/vida/vida-open"
+export CONAN_USERNAME="vida"
+export CONAN_LOGIN_USERNAME="erik"
+export CONAN_CHANNEL="stable"
+
+python3 build.py
