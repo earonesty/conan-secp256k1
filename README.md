77a7ca94eecc87a224ad39dcb80243330c71fd0a
erik aronesty <erik@q32.com>
2018-11-28 13:06:45 -0500

    .

---
+++ README.md
@@ -0,0 +1,10 @@
+# Conan packager for secp256k1
+
+Bitcoin's libsecp256k1 packaged for conan.io / cmake.  Mostly used as an example. 
+
+## Overview
+
+	1. Don't use binaries of this package that were uploaded to bintray, uninstead use your own internal conan repo, and use this packager to build it from source
+
+	2. To use, fork this repo, modify the the "upload.sh", "build.py" and other things with your own parameters.   This repo is mostly just an example of autotools->conan....!
+
--- build.py
+++ build.py
@@ -7,6 +7,7 @@ if __name__ == "__main__":
     builder = ConanMultiPackager(channel="stable")
 
     if sys.platform == "linux":
+        # cross-compile windows from linux... easier
         builder.add(settings={"os":"Windows", "arch": "x86_64", "build_type": "Debug"},
                     options={}, env_vars={}, build_requires={})
 
@@ -19,6 +20,7 @@ if __name__ == "__main__":
         builder.add(settings={"os":"Windows", "arch": "x86", "build_type": "Release"},
                     options={}, env_vars={}, build_requires={})
 
+    # debug/release 54 bit build
     builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
                 options={}, env_vars={}, build_requires={})
 
